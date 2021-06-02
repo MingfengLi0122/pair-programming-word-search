a551269c2ef4f92c397acc751f9a269d5b20b8f9
Mingfeng Li <lmf950122@gmail.com>
2021-06-02 22:44:48 +0000

    Word search

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,22 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    const transArr = letters[0].map((column, index) => letters.map(rows => rows[index])); 
+    const verticalJoin = transArr.map(ls => ls.join(''));
+    let isFound = false;
+
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        console.log("l.includes(word):",l.includes(word));
+        if (l.includes(word)) isFound = true;
     }
+    
+    if (isFound === true) return isFound;
+    
+    for (l of verticalJoin) {
+        if (l.includes(word)) isFound = true;
+    }
+    return isFound;
 }
 
-module.exports = wordSearch
+module.exports = wordSearch;
+
+
